2d658766d7b6f3eba2caf262b852b1cb72d52ba4
unknown <ARazumovskiy@mrk-i1435.amust.local>
2015-10-22 11:33:37 +0300

    upd

--- countryJson/src/main/scala/app.scala
+++ countryJson/src/main/scala/app.scala
@@ -1,3 +1,4 @@
+import java.io._
 import scala.collection.immutable.{ListMap}
 import scala.io.Source
 import net.liftweb.json._
@@ -7,44 +8,42 @@ import net.liftweb.json.Extraction._
 object app extends App {
   implicit val formats = net.liftweb.json.DefaultFormats
 
-//  case class Country(Area: Option[String] = Some(""), SalesTeam: String = "", SubRegion: String = "",
-//                     Region: String = "", GlobalRegion: String = "", Territory: String = "")
   case class Country(Area: Option[String] = None, SalesTeam: Option[String] = None, SubRegion: Option[String] = None,
                      Region: Option[String] = None, GlobalRegion: Option[String] = None, Territory: Option[String] = None)
   //val countryTest = Country(None, Some("STeam"), Some("SBReg"), Some("Regs"), Some("Greg"), Some("MyTerr"))
-  val countryTest = Country(GlobalRegion = Some("SBReg"))
+  //val countryTest = Country(GlobalRegion = Some("SBReg"))
 
   val sObject = "pmdf"
   val element = "GlobalRegion"
+  val outputFile = args(2)
 
-  //val countries = Source.fromFile(args(0)).getLines.map(_.replaceAll("\"", "")).filter(_.startsWith("Ang")).toList
+  //// making map Country -> Value to add into JSON
+  val countryString = Source.fromFile(args(1)).getLines.map(_.replaceAll("[ ]{2,20}", ":"))
+
+  val map = countryString.map(splitToTuple(_, ":")).toMap
+  //for (name <- map) println(name)
+  //println(map.get("Macau").get)
+
+
+  //// making json by finding values in map
   val countries = Source.fromFile(args(0)).getLines.map(_.replaceAll("\"", "")).toList
   //for (name <- countries) println(name)
 
-//  def mkJson2(country: String, section: String): (String, JObject) = {
-//    country -> (section -> decompose(countryTest))
-//  }
-//  val genTest2 = countries.map(c => mkJson2(c, "pmdf")).toMap
-//  println("genTest2:" + genTest2.getClass)
-//  println(pretty(render(decompose(genTest2))))
+  val jsonResult = ListMap(countries.map(c => (c, mkJson(Country(GlobalRegion = map.get(c)), sObject))): _*)
+  println(pretty(render(decompose(jsonResult))))
 
-  def mkJson(country: String, section: String): JObject = {
-    section -> decompose(countryTest)
-  }
+  val file = new File(outputFile)
+  val bw = new BufferedWriter(new FileWriter(file))
+  bw.write(pretty(render(decompose(jsonResult))))
+  bw.close()
 
-  val genTest = ListMap(countries.map(c => (c, mkJson(c, sObject))): _*)
-  println(pretty(render(decompose(genTest))))
-
-  val countriesSection = args(1)
-  val countryString = Source.fromFile(countriesSection).getLines.map(_.replaceAll("[ ]{2,20}", ":"))
-
-  val map = countryString.map(splitToTuple(_, ":")).toMap
-  println(map.size)
-  //for (name <- map) println(name)
-  println(map.get("Macau").get)
 
   def splitToTuple(s: String, splt: String): (String, String) = s.split(splt) match {
     case Array(x: String, y: String) => Tuple2(y, x)
+  }
+
+  def mkJson(country: Country, section: String): JObject = {
+    section -> decompose(country)
   }
 }
 
